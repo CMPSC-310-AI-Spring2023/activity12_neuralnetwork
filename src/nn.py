e009f77792bbd45a78746c58c2e420d0abccb0c2
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-03-18 21:39:02 -0400

    Update activity

--- src/nn.py
+++ src/nn.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 # Each row is a training example, each column is a feature  [X1, X2, X3]
-# 4 by 3 array
 X=np.array(([0,0,1],[0,1,1],[1,0,1],[1,1,1]), dtype=float)
 y=np.array(([0],[1],[1],[0]), dtype=float)
 
-# Activation function
+# Activation function - sigmoid function
 def sigmoid(t):
     return 1/(1+np.exp(-t))
 
@@ -13,14 +12,35 @@ def sigmoid(t):
 def sigmoid_derivative(p):
     return p * (1 - p)
 
-# Class definition
+#TODO: add comments
 class NeuralNetwork:
     def __init__(self, x, y):
-        TODO: complete implementation
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1], 4)
+        self.weights2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeros(y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        #print(self.layer1)
+        self.layer2 = sigmoid(np.dot(self.layer1, self.weights2))
+        return self.layer2
+
+    def backprop(self):
+        d_weights2 = np.dot(self.layer1.T, 2*(self.y - self.output)*sigmoid_derivative(self.output))
+        d_weights1 = np.dot(self.input.T, np.dot(2*(self.y - self.output)*sigmoid_derivative(self.output), self.weights2.T)*sigmoid_derivative(self.layer1))
+
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+    def train(self, X, y):
+        self.output = self.feedforward()
+        self.backprop()
 
 
 NN = NeuralNetwork(X, y)
-for i in range(1500):  # trains the NN 1,000 times
+for i in range(1000):  # trains the NN 1,000 times
     if i % 100 == 0:
         print("for iteration # " + str(i) + "\n")
         print("Input : \n" + str(X))
